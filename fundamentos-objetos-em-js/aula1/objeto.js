ef926429c2ac4053ccaa193be47de2974bad5d1b
Pedro Felipe Paulino Rosinha <121513464+pedrorosinha@users.noreply.github.com>
2023-12-17 21:36:06 -0300

    Adicionando curso de fundamentos-objetos-em-js

---
+++ fundamentos-objetos-em-js/aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = [111111, 222222, 333333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11112222333"];
+
+console.log(informacoesPessoa[1])
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+}
